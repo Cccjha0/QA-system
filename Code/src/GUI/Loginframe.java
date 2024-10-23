a52140d6f9e98e5d74fb2a158f23c91c89cdf1e1
ponder <2161219478@qq.com>
2024-10-23 08:39:21 +0800

    GUI update#2

--- Code/src/GUI/Loginframe.java
+++ Code/src/GUI/Loginframe.java
@@ -14,25 +14,35 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Loginframe {
+    static JFrame loginframe = new JFrame("Login");
 
-    public static void main(String[] args) {
-        JFrame loginframe = new JFrame("Login");
+    Toolkit kit = Toolkit.getDefaultToolkit();
+    Dimension screenSize = kit.getScreenSize();
+    int screenWidth = screenSize.width; //屏幕宽度
+    int screenHeight = screenSize.height; //屏幕高度
+
+    static JPanel mainpanel = new JPanel(new CardLayout());
+    JPanel loginpanel = new JPanel();
+    JPanel registerpanel = new JPanel();
+    public Loginframe(){
         loginframe.setSize(400, 250);
         loginframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = kit.getScreenSize();
-        int screenWidth = screenSize.width; //屏幕宽度
-        int screenHeight = screenSize.height; //屏幕高度
         loginframe.setLocation((screenWidth-400)/2, (screenHeight-250)/2);
 
-        JPanel loginpanel = new JPanel();
-        loginframe.add(loginpanel);
+        mainpanel.add(loginpanel,"login");
+        mainpanel.add(registerpanel,"Register");
+        registerpanelComponents(registerpanel);
         loginComponents(loginpanel);
+
+        loginframe.add(mainpanel);
         loginframe.setVisible(true);
     }
+    public static void main(String[] args) {
+        Loginframe a = new Loginframe();
+    }
 
-    private static void loginComponents(JPanel panel) {
+    private void loginComponents(JPanel panel) {
         panel.setLayout(null);
 
         JLabel userLabel = new JLabel("ID:");
@@ -51,38 +61,87 @@ public class Loginframe {
         passwordText.setBounds(100,80,240,25);
         panel.add(passwordText);
 
+        JLabel failedLabel = new JLabel("Failed login, ID or password is incorrect.");
+        failedLabel.setBounds(95,170,300,25);
         // 登录按钮
         JButton loginButton = new JButton("Login");
-        loginButton.setBounds(100, 140, 80, 40);
+        loginButton.setBounds(100, 120, 100, 40);
         panel.add(loginButton);
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //测试 user：ponder  password：12345
+                panel.remove(failedLabel);//删除之前的报错标签
                 if(userText.getText().equals("ponder")&& new String(passwordText.getPassword()).equals("12345")) {
-                    //关闭loginframe，打开QAFrame，当关闭QAFrame时，再次打开loginframe
-
+                    //关闭loginframe
+                    loginframe.dispose();
                     QAframe.QAComponents();
+
                 } else {
-                    JOptionPane.showConfirmDialog(null,"登陆失败");}
+                    panel.add(failedLabel);
+                    panel.validate();
+                    panel.repaint();
+                }
             }
         });
-
-        //重置按钮
-        JButton resetButton = new JButton("Reset");
-        resetButton.setBounds(220,140,80,40);
-        panel.add(resetButton);
-        resetButton.addActionListener(new ActionListener() {
+        //注册按钮
+        JButton registerButton = new JButton("Register");
+        registerButton.setBounds(240,120,100,40);
+        panel.add(registerButton);
+        registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(userText!=null);
-                userText.setText(null);
-                if(passwordText!=null);
-                passwordText.setText(null);
+                CardLayout cl = (CardLayout)(mainpanel.getLayout());
+                cl.show(mainpanel, "Register");
+                loginframe.setTitle("Register");
             }
         });
     }
 
+    public static void registerpanelComponents(JPanel registerpanel){
+        registerpanel.setLayout(null);
+
+        JLabel userLabel = new JLabel("Set ID:");
+        userLabel.setBounds(10,40,80,25);
+        registerpanel.add(userLabel);
+        //输入ID的文本框
+        JTextField userText = new JTextField(20);
+        userText.setBounds(110,40,240,25);
+        registerpanel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Set Password:");
+        passwordLabel.setBounds(10,90,100,25);
+        registerpanel.add(passwordLabel);
+        //输入密码的文本框
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(110,90,240,25);
+        registerpanel.add(passwordText);
+        // 登录按钮
+        JButton registerButton = new JButton("Register and Login");
+        registerButton.setBounds(50, 140, 140, 40);
+        registerpanel.add(registerButton);
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginframe.dispose();
+                QAframe.QAComponents();
+            }
+        });
+
+        JButton CancelButton = new JButton("Cancel");
+        CancelButton.setBounds(210, 140, 140, 40);
+        CancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout)(mainpanel.getLayout());
+                cl.show(mainpanel, "login");
+                loginframe.setTitle("Login");
+            }
+        });
+        registerpanel.add(CancelButton);
+    }
 }
+
+
 
 
--- Code/src/GUI/QAframe.java
+++ Code/src/GUI/QAframe.java
@@ -9,79 +9,208 @@ package GUI;
  * @author peter
  */
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class QAframe {
+    static Font font = new Font("Arial",Font.PLAIN,20);
+    static JFrame QAframe;
     public static void main(String[] args) {
-        QAframe.QAComponents();
+        QAComponents();
     }
     public static void QAComponents() {
-        JFrame QAframe = new JFrame("QA_Syetem");
+        //窗口设置
+        QAframe = new JFrame("QA_Syetem");
         QAframe.setSize(1000, 700);
         QAframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension screenSize = kit.getScreenSize();
         int screenWidth = screenSize.width; //屏幕宽度
         int screenHeight = screenSize.height; //屏幕高度
         QAframe.setLocation((screenWidth-1000)/2, (screenHeight-700)/2);
 
-        JPanel QApanel = new JPanel();
-        QAframe.add(QApanel);
-        QApanelComponents(QApanel);
-        QAframe.setVisible(true);
-    }
+        //面板设置
+        JPanel QAPanel = new JPanel(new CardLayout());
+        JPanel queryPanel = new JPanel();
+        JPanel inputPanel = new JPanel();
+        QAPanel.add(queryPanel,"query");
+        QAPanel.add(inputPanel,"input");
 
-    private static void QApanelComponents(JPanel qapanel) {
-        qapanel.setLayout(null);
-
-        JLabel userLabel = new JLabel("User name");
-        userLabel.setBounds(10,10,80,25);
-        qapanel.add(userLabel);
-
-        JTextField questiomText = new JTextField(20);
-        questiomText.setBounds(190,50,600,30);
-        qapanel.add(questiomText);
-
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
         JButton queryButton = new JButton("Query");
-        queryButton.setBounds(800, 48, 80, 35);
-        qapanel.add(queryButton);
+        JButton inputButton =new JButton("Input");
+
+        Dimension buttonSize = new Dimension(100, 30);
+        queryButton.setPreferredSize(buttonSize);
+        queryButton.setMinimumSize(buttonSize);
+        queryButton.setMaximumSize(buttonSize);
+        inputButton.setPreferredSize(buttonSize);
+        inputButton.setMinimumSize(buttonSize);
+        inputButton.setMaximumSize(buttonSize);
+
+        toolBar.add(queryButton);
+        toolBar.add(inputButton);
+
+        QAframe.add(toolBar,BorderLayout.NORTH);
+        QApanelComponents(queryPanel);
+        InputPanelComponents(inputPanel);
+
+        QAframe.add(QAPanel);
+        QAframe.setVisible(true);
+
         queryButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JTextArea jTextArea = new JTextArea(30,80);
-                jTextArea.setText("答案：略");
-                JScrollPane jScrollPane = new JScrollPane(jTextArea);
-                jScrollPane.setBounds(190,130,600,450);
-                qapanel.add(jScrollPane);
-
-                qapanel.validate();
+                CardLayout cl = (CardLayout)(QAPanel.getLayout());
+                cl.show(QAPanel,"query");
             }
         });
 
-        JButton inputButton = new JButton("Input");
-        inputButton.setBounds(890, 48, 80, 35);
-        qapanel.add(inputButton);
         inputButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                JTextArea jTextArea = new JTextArea(30,80);
-                jTextArea.setText("请输入答案");
-                JScrollPane jScrollPane = new JScrollPane(jTextArea);
-                jScrollPane.setBounds(190,130,600,450);
-                qapanel.add(jScrollPane);
-
-                JButton inputButton2 = new JButton("Input");
-                inputButton2.setBounds(800, 130, 80, 35);
-                qapanel.add(inputButton2);
-
-                qapanel.validate();
+                CardLayout cl = (CardLayout)(QAPanel.getLayout());
+                cl.show(QAPanel, "input");
             }
         });
-
-
-
     }
+
+    private static void InputPanelComponents(JPanel inputPanel) {
+        inputPanel.setLayout(null);
+        JLabel userLabel = new JLabel("User name");
+        userLabel.setBounds(10,10,80,25);
+        inputPanel.add(userLabel);
+
+        JLabel qLabel = new JLabel("Input your question:");
+        qLabel.setFont(font);
+        qLabel.setBounds(150, 20, 200, 25);
+        inputPanel.add(qLabel);
+
+        JTextArea qArea = new JTextArea(20, 55);
+        JScrollPane qjScrollPane = new JScrollPane(qArea);
+        qjScrollPane.setBounds(150, 55, 700, 100);
+        inputPanel.add(qjScrollPane);
+        qArea.setLineWrap(true);
+        qArea.setWrapStyleWord(true);
+        qArea.setRows(qArea.getColumns());
+        qArea.revalidate();
+
+        JLabel aLabel = new JLabel("Input your answer:");
+        aLabel.setFont(font);
+        aLabel.setBounds(150, 170, 200, 25);
+        inputPanel.add(aLabel);
+
+        JTextArea aArea = new JTextArea(30, 55);
+        JScrollPane ajScrollPane = new JScrollPane(aArea);
+        ajScrollPane.setBounds(150, 215, 700, 330);
+        inputPanel.add(ajScrollPane);
+        aArea.setLineWrap(true);
+        aArea.setWrapStyleWord(true);
+        aArea.setRows(aArea.getColumns());
+        aArea.revalidate();
+
+        JButton inputButton = new JButton("Input");
+        inputButton.setBounds(770, 555, 80, 35);
+        inputPanel.add(inputButton);
+        inputButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (qArea.getText().trim().isEmpty()){
+                    JLabel Label = new JLabel("Plese input question");
+                    Label.setFont(font);
+                    Label.setBounds(150, 555, 300, 25);
+                    inputPanel.add(Label);
+                }else if (aArea.getText().trim().isEmpty()){
+                    JLabel Label = new JLabel("Plese input answer");
+                    Label.setFont(font);
+                    Label.setBounds(150, 555, 300, 25);
+                    inputPanel.add(Label);
+
+                }
+                else {}
+                inputPanel.revalidate();
+                inputPanel.repaint();
+            }
+        });
+        JButton quitButton = new JButton("Quit");
+        quitButton.setBounds(20, 590, 80, 35);
+        inputPanel.add(quitButton);
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                QAframe.dispose();
+            }
+        });
+    }
+
+    private static void QApanelComponents(JPanel queryPanel) {
+        queryPanel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User name");
+        userLabel.setBounds(10,10,80,25);
+        queryPanel.add(userLabel);
+
+        JLabel qLabel = new JLabel("Query your question:");
+        qLabel.setFont(font);
+        qLabel.setBounds(150, 20, 200, 25);
+        queryPanel.add(qLabel);
+
+        JTextArea qArea = new JTextArea(20, 55);
+        JScrollPane qjScrollPane = new JScrollPane(qArea);
+        qjScrollPane.setBounds(150, 55, 700, 100);
+        queryPanel.add(qjScrollPane);
+        qArea.setLineWrap(true);
+        qArea.setWrapStyleWord(true);
+        qArea.setRows(qArea.getColumns());
+        qArea.revalidate();
+
+        JButton queryButton = new JButton("Query");
+        queryButton.setBounds(145, 165, 80, 35);
+        queryPanel.add(queryButton);
+        queryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPanel rollpanel = new JPanel();
+                rollpanel.setLayout(new BoxLayout(rollpanel, BoxLayout.Y_AXIS));
+                genTextArea(rollpanel,5);
+                JScrollPane scrollPane = new JScrollPane(rollpanel);
+                queryPanel.add(scrollPane);
+                scrollPane.setBounds(145,215,700,400);
+                queryPanel.revalidate();
+                queryPanel.repaint();
+            }
+        });
+        JButton quitButton = new JButton("Quit");
+        quitButton.setBounds(20, 590, 80, 35);
+        queryPanel.add(quitButton);
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                QAframe.dispose();
+            }
+        });
+    }
+
+    public static void genTextArea(JPanel panel, int n){
+        for (int i = 0; i < n; i++) {
+            JTextArea textArea = new JTextArea(3, 55);
+            JScrollPane jScrollPane = new JScrollPane(textArea);
+            jScrollPane.setPreferredSize(new Dimension(700,100));
+            panel.add(jScrollPane);
+            if (i < n - 1) {
+                panel.add(Box.createVerticalStrut(20));
+            }
+            textArea.setLineWrap(true); // 自动换行
+            textArea.setWrapStyleWord(true); // 按单词换行
+            textArea.setRows(textArea.getColumns());
+            textArea.revalidate();
+        }
+        panel.revalidate();
+        panel.repaint();
+        panel.setVisible(true);
+    }
+
 }
